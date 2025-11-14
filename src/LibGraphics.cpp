a2e9ac566071b56b84ee0c5a9b8c97e9e61e18b7
Johnny Mast <mastjohnny@gmail.com>
2025-11-14 14:34:12 +0100

    updated rect to coords

--- src/LibGraphics.cpp
+++ src/LibGraphics.cpp
@@ -7,17 +7,8 @@ namespace LibGraphics {
     OpenCVInfo GetOpenCVInfo() {
 
         bool haveCuda = false;
-        int deviceCount = cv::cuda::getCudaEnabledDeviceCount();
-        if (deviceCount > 0) {
-
-            for (int i = 0; i < deviceCount; ++i) {
-                cv::cuda::DeviceInfo devInfo(i);
-                std::cout << "Device " << i << ": " << devInfo.name()
-                          << " (Compute capability " << devInfo.majorVersion()
-                          << "." << devInfo.minorVersion() << ")" << std::endl;
-            }
-        } else {
-            std::cout << "No CUDA devices found or OpenCV not built with CUDA." << std::endl;
+        if (int deviceCount = cv::cuda::getCudaEnabledDeviceCount(); deviceCount > 0) {
+            haveCuda = true;
         }
 
         return OpenCVInfo{CV_VERSION_MAJOR, CV_VERSION_MINOR, CV_VERSION_REVISION, haveCuda};
