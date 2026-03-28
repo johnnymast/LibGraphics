5033dbeca4244e0110346fe870a072bd62fa6664
Johnny Mast <mastjohnny@gmail.com>
2026-03-28 15:15:42 +0100

    forgot to add Tesseract back in the linking line.

--- cmake/Tesseract.cmake
+++ cmake/Tesseract.cmake
@@ -19,4 +19,6 @@ set_target_properties(libtesseract PROPERTIES POSITION_INDEPENDENT_CODE ON)
 
 add_library(Tesseract::Tesseract ALIAS libtesseract)
 
+
+
 message(STATUS "Tesseract OCR setup complete")
